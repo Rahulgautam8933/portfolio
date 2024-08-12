48471d84d5b79ee2c744434bf9be7aa435d3d2bc
rahul <rahulsoftfix@gmail.com>
2024-08-12 18:39:37 +0530

    tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/compoments/Navbar";
+import Footer from "@/compoments/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          {children}
+        </div>
+        <div>
+          <Footer />
+        </div>
+
+      </body>
     </html>
   );
 }
--- src/compoments/Navbar/index.js
+++ src/compoments/Navbar/index.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
 const Navbar = () => {
     return (
-        <div>navbar</div>
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Link</a></li>
+                        <li>
+
+                            <details>
+                                <summary>Parent</summary>
+                                <ul className="bg-base-100 rounded-t-none p-2">
+                                    <li><a>Link 1</a></li>
+                                    <li><a>Link 2</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='text-red-200'>Hello there</div>
+
+        </div>
     )
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
